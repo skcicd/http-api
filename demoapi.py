eedef40017cb45466681f868d9f7ade87766a9b1
Sergey from work <skcicd@gmail.com>
2024-11-14 08:32:07 +0200

    add users dic

--- demoapi.py
+++ demoapi.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+
+users = {
+    "1": {'id':1, "name":"John"},
+    "2": {'id':2, "name":"Mary"}
+}
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET'])
+def top_url():
+    temp = list(users.values())
+    return jsonify(temp)
 
 
-app.run()
+app.run(host='0.0.0.0', port=5000, debug=True)
